7aa1182242797dd504b737654ee3d8a2ea3c893a
Jakub Jakubowski <j.jakubowski13@gmail.com>
2021-04-25 23:41:58 +0200

    responsivnes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+/* Navigation Events */
+
+function showNavigation(){
+  let showHamburger = document.getElementById('hamburger')
+  showHamburger.style.display = 'none'
+}
+
+
+/* Footer Year */
 document.getElementById('year').innerHTML = new Date().getFullYear();
