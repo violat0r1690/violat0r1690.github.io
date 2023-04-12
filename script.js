ce395347e437baf9f9b4aeff11e6edb7f49ad1eb
violat0r1690 <jakub.przemyslaw.jakubowski@gmail.com>
2023-04-12 13:32:14 +0200

    carusel sandbox

--- script.js
+++ script.js
@@ -6,6 +6,37 @@ function showNavigation(){
   console.log('Hello World!')
 }
 
+/*Carusel Scipts*/
+
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+
 
 /* Footer Year */
 document.getElementById('year').innerHTML = new Date().getFullYear();
